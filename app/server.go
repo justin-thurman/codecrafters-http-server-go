6f57103bafaeb38d279c0a1be95fa80bce86df98
Justin Thurman <justin.thurman@bevy.com>
2024-06-07 10:44:58 -0400

    Print out request bytes

--- app/server.go
+++ app/server.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -21,5 +19,12 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	var request []byte
+	_, err = conn.Read(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", request)
+
+	// conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
